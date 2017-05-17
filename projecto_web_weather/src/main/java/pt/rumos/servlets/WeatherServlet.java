d84d83886b75475bcdee6e9be24693de2c47beaa
Mauro Barros <barros.maur@gmail.com>
2017-05-17 21:42:14 +0100

    + conversor de temperatura!!

--- projecto_web_weather/src/main/java/pt/rumos/servlets/WeatherServlet.java
+++ projecto_web_weather/src/main/java/pt/rumos/servlets/WeatherServlet.java
@@ -7,9 +7,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.swing.ImageIcon;
 import pt.rumos.services.WeatherApiService;
 import pt.rumos.entities.CityWeatherResponse;
+import pt.rumos.utils.Converter;
+
 
 @WebServlet(name = "WeatherServlet", urlPatterns = {"/WeatherServlet"})
 public class WeatherServlet extends HttpServlet {
@@ -36,6 +37,19 @@ public class WeatherServlet extends HttpServlet {
         w.println("WEATHER FORECAST: " + cwr.getWeather().get(0).getDescription() + "</br>");
         w.println("Main: " + cwr.getWeather().get(0).getMain() + "</br>");
         w.println("Image: <img src=" + cwr.getWeather().get(0).getIcon() + ">");
+       
+       
+        w.println("<br/>HUMIDICITY :"+cwr.getMain().getHumidity()+"<br/>");
+        w.println("PRESSURE :"+cwr.getMain().getPressure()+"<br/><br/>");
+        w.println("TEMP (Fº) :"+cwr.getMain().getTemp()+" Fº<br/>");
+        w.println("TEMP_MAX (Fº) :"+cwr.getMain().getTemp_max()+" Fº<br/>");
+        w.println("TEMP_min (Fº) :"+cwr.getMain().getTemp_min()+" Fº<br/><br/>");
+     
+        
+      
+        w.println("TEMP (Cº) :"+Converter.convertToCelsius(cwr.getMain().getTemp())+"<br/>");
+        w.println("TEMP_MAX (Cº) :"+Converter.convertToCelsius(cwr.getMain().getTemp_max())+"Cº <br/>");
+        w.println("TEMP_min (Cº) :"+Converter.convertToCelsius(cwr.getMain().getTemp_min())+"Cº <br/>");
         w.println("</body></html>");
         
          
