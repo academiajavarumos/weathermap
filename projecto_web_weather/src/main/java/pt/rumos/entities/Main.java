4786a73ee12b9a721d613646ae559b8764427ba9
Paulo Teixeira <plopesteixeira@outlook.com>
2017-05-18 23:43:35 +0100

    tentativas de quarta-feira ainda não consegui ler o ficheiro diz que não encontra

---
+++ projecto_web_weather/src/main/java/pt/rumos/entities/Main.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pt.rumos.entities;
+
+/**
+ *
+ * @author maurobarros
+ */
+public class Main {
+    private double temp;
+    private int pressure;
+    private double humidity;
+    private double temp_min;
+    private double temp_max;
+
+    public double getTemp() {
+        return temp;
+    }
+
+    public void setTemp(double temp) {
+        this.temp = temp;
+    }
+
+    public int getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(int pressure) {
+        this.pressure = pressure;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+
+    public double getTemp_min() {
+        return temp_min;
+    }
+
+    public void setTemp_min(double temp_min) {
+        this.temp_min = temp_min;
+    }
+
+    public double getTemp_max() {
+        return temp_max;
+    }
+
+    public void setTemp_max(double temp_max) {
+        this.temp_max = temp_max;
+    }
+    
+    
+    
+}
--- projecto_web_weather/src/main/java/pt/rumos/servlets/WeatherServlet.java
+++ projecto_web_weather/src/main/java/pt/rumos/servlets/WeatherServlet.java
@@ -2,15 +2,15 @@ package pt.rumos.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.swing.ImageIcon;
 import pt.rumos.services.WeatherApiService;
 import pt.rumos.entities.CityWeatherResponse;
+import pt.rumos.utils.Converter;
+
 
 @WebServlet(name = "WeatherServlet", urlPatterns = {"/WeatherServlet"})
 public class WeatherServlet extends HttpServlet {
@@ -22,16 +22,12 @@ public class WeatherServlet extends HttpServlet {
     //*****************************
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String StringImg;
         String city = request.getParameter("city");
         String country = request.getParameter("country");
 
         //pedido http para weather api
         WeatherApiService was = new WeatherApiService();
-
-        List ct = was.getCitiesList();
-        
-        
         CityWeatherResponse cwr = was.getWeatherObj(city, country);
         
         //resposta
@@ -41,13 +37,22 @@ public class WeatherServlet extends HttpServlet {
         w.println("WEATHER FORECAST: " + cwr.getWeather().get(0).getDescription() + "</br>");
         w.println("Main: " + cwr.getWeather().get(0).getMain() + "</br>");
         w.println("Image: <img src=" + cwr.getWeather().get(0).getIcon() + ">");
+       
+       
+        w.println("<br/>HUMIDICITY :"+cwr.getMain().getHumidity()+"<br/>");
+        w.println("PRESSURE :"+cwr.getMain().getPressure()+"<br/><br/>");
+        w.println("TEMP (Fº) :"+cwr.getMain().getTemp()+" Fº<br/>");
+        w.println("TEMP_MAX (Fº) :"+cwr.getMain().getTemp_max()+" Fº<br/>");
+        w.println("TEMP_min (Fº) :"+cwr.getMain().getTemp_min()+" Fº<br/><br/>");
+     
+        
+      
+        w.println("TEMP (Cº) :"+Converter.convertToCelsius(cwr.getMain().getTemp())+"<br/>");
+        w.println("TEMP_MAX (Cº) :"+Converter.convertToCelsius(cwr.getMain().getTemp_max())+"Cº <br/>");
+        w.println("TEMP_min (Cº) :"+Converter.convertToCelsius(cwr.getMain().getTemp_min())+"Cº <br/>");
         w.println("</body></html>");
         
+         
         
-        
-
     }
-    
-    
-
 }
